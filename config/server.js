bdec252672b06febe6462c4f093119d960411a96
iiboy-2022099 <iiboy-2022099@kinal.edu.gt>
2024-03-07 22:46:48 -0600

    Subiendo login

--- config/server.js
+++ config/server.js
@@ -6,12 +6,14 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import { dbConnection } from './mongo.js';
 import userRoutes from '../src/user/user.routes.js';
+import authRoutes from '../src/auth/auth.routes.js';
 
 class Server{
     constructor(){
         this.app = express();
         this.port = process.env.PORT;
         this.usuarioPath = '/ProyectSystem/v2/users';
+        this.authPath = '/ProyectSystem/v2/auth';
 
         this.middlewares();
         this.conectarDB();
@@ -32,6 +34,7 @@ class Server{
 
     routes(){
         this.app.use(this.usuarioPath, userRoutes);
+        this.app.use(this.authPath, authRoutes);
     }
 
     listen(){
--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,41 +1,43 @@
 import bcryptjs from 'bcryptjs';
 import User from '../user/user.model.js'
-import { generarJWT } from '../helpers/generate-jwt.js'; 
+import { generarJWT } from '../helpers/generate-jwt.js';
 
 export const login = async (req, res) => {
-    const { email, password } = req.body; 
+    const { email, password } = req.body;
 
-  try {
-    const user = await User.findOne({ email });
+    try {
+        const user = await User.findOne({ email });
 
-    if (!user) {
-      return res.status(400).json({
-        msg: "Credenciales incorrectas, Correo no existe en la base de datos",
-      });
+        if (!user) {
+            return res.status(400).json({
+
+                msg: "Credenciales incorrectas, Correo no existe en la base de datos",
+
+            });
+        }
+        if (!user.estado) {
+            return res.status(400).json({
+                msg: "El usuario no existe en la base de datos",
+            });
+        }
+        const validPassword = bcryptjs.compareSync(password, user.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                msg: "La contraseña es incorrecta",
+            });
+        }
+        const token = await generarJWT(user.id);
+
+        res.status(200).json({
+            msg: 'Login Ok!!!',
+            user,
+            token
+        });
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            msg: "Comuniquese con el administrador",
+        });
     }
-    if (!user.estado) {
-      return res.status(400).json({
-        msg: "El usuario no existe en la base de datos",
-      });
-    }
-    const validPassword = bcryptjs.compareSync(password, user.password);
-    if (!validPassword) {
-      return res.status(400).json({
-        msg: "La contraseña es incorrecta",
-      });
-    }
-    const token = await generarJWT( user.id);
-
-    res.status(200).json({
-      msg: 'Login Ok!!!',
-      user,
-      token
-    });
-
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({
-      msg: "Comuniquese con el administrador",
-    });
-  }
 }
